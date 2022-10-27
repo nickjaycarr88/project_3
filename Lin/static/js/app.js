64ab1ae10b6c4339aaede8295952046e2d317434
LynHJ <105850558+LynHJ@users.noreply.github.com>
2022-10-27 20:40:28 +0800

    update

--- Lin/static/js/app.js
+++ Lin/static/js/app.js
@@ -4,21 +4,21 @@ function valueColor() {
     element = document.getElementById("color");
     if (eval(rate) === eval(0))    //js cannot tell the numbers or text, so use eval() to campare numbers
         {element.style.color = '#6c6c6c';}
-    else if (eval(rate) <eval(5))  
+    else if (eval(rate) > eval(0) && eval(rate) <eval(5))  
         {element.style.color = '#79ff79';}
-    else if (eval(rate) <eval(10))  
+    else if (eval(rate)>=eval(5) && eval(rate) <eval(10))  
         {element.style.color = '#00db00';}   
-    else if (eval(rate) < eval(15))  
+    else if (eval(rate)>=eval(10) && eval(rate) < eval(15))  
         {element.style.color = '#009100';}
-    else if (eval(rate) < eval(20) ) 
+    else if ( eval(rate)>=eval(15) && eval(rate) < eval(20) ) 
         {element.style.color = '#006000';}
-    else if (eval(rate) >= eval(-5)) 
+    else if (eval(rate)<eval(0) && eval(rate) >= eval(-5)) 
         {element.style.color = '#ff2d2d';}
-    else if (eval(rate) > eval(-10)) 
+    else if (eval(rate) <= eval(-5) && eval(rate) > eval(-10)) 
         {element.style.color = '#ea0000';}    
-    else if (eval(rate) >eval( -15)) 
+    else if (val(rate) <= eval(-10) && eval(rate) >eval(-15)) 
         {element.style.color = '#ae0000';}
-    else if(eval(rate) > eval(-21))  
+    else if(val(rate) <= eval(-15) && eval(rate) > eval(-21))  
         {element.style.color = '#600000';} 
 }
 
